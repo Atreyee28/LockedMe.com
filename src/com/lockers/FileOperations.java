e5ee7d909e79b45e87d11dffad7d4b2aa90dad23
Atreyee Mondal (atrmonda) <atrmonda@cisco.com>
2021-10-07 11:46:01 +0530

    Changes Saved

--- src/com/lockers/FileOperations.java
+++ src/com/lockers/FileOperations.java
@@ -1,4 +1,5 @@
 package com.lockers;
+
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -10,8 +11,7 @@ import java.util.List;
 import java.util.Scanner;
 
 public class FileOperations {
-	
-	//returns the current file names in ascending order 
+	//This function will show all the files in ascending order.
 	public static void RecursivePrint(File[] arr,int index,int level) 
     {
         // terminate condition
@@ -37,95 +37,81 @@ public class FileOperations {
         // recursion for main directory
         RecursivePrint(arr,++index, level);
    }
-
 	//add user specified files to the existing directory
-	public static void createFileUsingFileClass(String fileName) throws IOException
-    {
-          File file = new File(fileName);
-          //Create the file
-          if (file.createNewFile()){
-            System.out.println("File is added!");
-            //Write Content from UI
-	          FileWriter writer = new FileWriter(file);
-	          System.out.println("Type Yes to write content to the file:");
-	          Scanner s=new Scanner(System.in);
-	          String response=s.next();
-	          if(response.equals("Yes")||response.equals("yes")||response.equals("YES"))
-	          {
-	          System.out.println("Enter File Content:");
-	          Scanner sc=new Scanner(System.in);
-	          writer.write(sc.nextLine());
-	          writer.close();
+		public static void createFileUsingFileClass(String fileName) throws IOException
+	    {
+	          File file = new File(fileName);
+	          //Create the file
+	          if (file.createNewFile()){
+	            System.out.println("File is added!");
+	            //Write Content from UI
+		          FileWriter writer = new FileWriter(file);
+		          System.out.println("Type Yes to write content to the file:");
+		          Scanner s=new Scanner(System.in);
+		          String response=s.next();
+		          if(response.equals("Yes")||response.equals("yes")||response.equals("YES"))
+		          {
+		          System.out.println("Enter File Content:");
+		          Scanner sc=new Scanner(System.in);
+		          writer.write(sc.nextLine());
+		          writer.close();
+		          }
+		          else
+		          {
+		        	  System.out.println("File content not written.");
+		          }
+	          	}
+	          else{
+	            //if the file name already exist in the directory,the application will show it.
+	        	  System.out.println("File already exists.");
 	          }
-	          else
-	          {
-	        	  System.out.println("File content not written.");
-	          }
-          	}
-          else{
-            //if the file name already exist in the directory,the application will show it.
-        	  System.out.println("File already exists.");
-          }
-    }
-	//this function is used to read file contents
-	public static List<String> readFileInList(String fileName) 
-	  {  
-	    List<String> lines = Collections.emptyList(); 
-	    try
-	    { 
-	      lines = Files.readAllLines(Paths.get(fileName), StandardCharsets.UTF_8); 
-	    } 
-	  
-	    catch (IOException e) 
-	    { 
-	    	System.err.println(e.getMessage());
-	    } 
-	    return lines; 
-	  } 
-	//this function is used to delete the user specified file from the existing directory.
-	public static void delete(String filename)
-	{
-	boolean a;
-	try {
-		a = Files.deleteIfExists(Paths.get(filename));
-	  	//if file found in the directory,it will get deleted.
-		if (a == true)
-       	{
-       		System.out.println(filename + "-Deleted Successfully.");
-       	}
-       	//if the user specified file is not in the directory it will show file not found.
-		else
-       	{
-       		System.out.println("File not Found");
-       	}
-	} catch (IOException e) {
-		System.err.println(e.getMessage());
-	}
-	}
-
-	//This function is used to search a user specified file in the current directory
-	public static void search(String filename2) {
-		File file = new File(filename2);
-			if (file.exists())
-				{
-				System.out.println("File found in application");
-				System.out.println("Want to read the content of the file? Type yes to continue or no to exit from application");
-			    Scanner s1=new Scanner(System.in);
-			    String response=s1.next();
-			    if(response.equals("Yes")||response.equals("YES")||response.equals("yes")) {} 
-			    else if (response.equals("No")||response.equals("NO")||response.equals("no"))
-			     	{
-			     		System.exit(0);
-			     	}
-			    else
-			     	{
-			     		System.err.println("Invalid Input");
-			     	}
-			}
-			  else
-			     {
-			    	 System.out.println("File not Found");
-			     }
-		
-        }
+	    }
+		//this function is used to read file contents
+		public static List<String> readFileInList(String fileName) 
+		  {  
+		    List<String> lines = Collections.emptyList(); 
+		    try
+		    { 
+		      lines = Files.readAllLines(Paths.get(fileName), StandardCharsets.UTF_8); 
+		    } 
+		  
+		    catch (IOException e) 
+		    { 
+		    	System.err.println(e.getMessage());
+		    } 
+		    return lines; 
+		  } 
+		//this function is used to delete the user specified file from the existing directory.
+		public static void delete(String filename)
+		{
+		boolean a;
+		try {
+			a = Files.deleteIfExists(Paths.get(filename));
+		  	//if file found in the directory,it will get deleted.
+			if (a == true)
+	       	{
+	       		System.out.println(filename + "-Deleted Successfully.");
+	       	}
+	       	//if the user specified file is not in the directory it will show file not found.
+			else
+	       	{
+	       		System.out.println("File not Found");
+	       	}
+		} catch (IOException e) {
+			System.err.println(e.getMessage());
+		}
+		}
+		//This function will return the file if found while searching.
+		public static void search(String filename) {
+	        File folder_name=new File (System.getProperty("user.dir"));
+			String[] list = folder_name.list();
+	        for (String file: list) {
+	            if (filename.equals(file)) {
+	                System.out.println("File Found : File " + filename + " exists at " + folder_name);
+	                return;
+	            }
+	        }
+	        System.out.println("File not Found");
+			     
+	    }
 }
--- src/com/lockers/Test.java
+++ src/com/lockers/Test.java
@@ -7,135 +7,115 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Test {
-
-	public static void main(String[] args) {
-		//Welcome the user and show the application name
+	void showMainMenu() {
+        System.out.println("--------Main Menu--------"); //showing the main menu
+		System.out.println("1. View the list of files inside current directory \n2. View UI options \n3. Exit from Application");
+		System.out.println("Enter your choice:");
+        try{
+            Scanner s1 = new Scanner(System.in);
+            int option = s1.nextInt();
+            switch (option){
+                case 1 : {
+                	String maindirpath = System.getProperty("user.dir");
+                    
+        	        // File object
+        	        File maindir = new File(maindirpath);
+        	           
+        	        if(maindir.exists() && maindir.isDirectory())
+        	        {
+        	            // array for files and sub-directories 
+        	            // of directory pointed by main_directory
+        	            File arr[] = maindir.listFiles();
+        	              
+        	            System.out.println("**********************************************");
+        	            System.out.println("All Files from current working directory (in ascending order) : " + maindir);
+        	            System.out.println("**********************************************");
+        	              
+        	            // Calling recursive method
+        	            FileOperations.RecursivePrint(arr,0,0); 
+        	       }	
+                    showMainMenu();
+                }
+                case 2 : {
+                    showSubMenu();
+                }
+                case 3 : {
+                    System.out.println("Thank You. End of application.");
+                    System.exit(0);
+                }
+                default:
+                	System.out.println("Invalid Input. Showing main menu...");
+                	showMainMenu();
+            }
+        }
+        catch (Exception e){
+            System.out.println("Please enter 1, 2 or 3");
+            showMainMenu();
+        }
+    }
+	void showSubMenu() {
+		System.out.println("A. Add a file \nB. Delete a specific file \nC. Search for a specific file \nD. Main Memu ");
+    	System.out.println("Enter your choice:");
+            Scanner s2 = new Scanner(System.in);
+            String c= s2.next();
+            switch (c){
+                case "A" : 
+                {
+                	Scanner s3=new Scanner(System.in);
+        			System.out.println("Enter File Name:");
+        	        String filename=s3.next();
+        	        try {
+        				FileOperations.createFileUsingFileClass(filename);
+        			} catch (IOException e) {
+        				System.err.println(e.getMessage());
+        			}
+        	        List l = FileOperations.readFileInList(filename); 
+        		    Iterator<String> itr = l.iterator(); 
+        		    while (itr.hasNext()) 
+        		    	{
+        		    	System.out.println("The content of the file is:"+itr.next()); 	
+        		    	}
+        	    	break;
+                }
+                case "B" : 
+                	{
+    	    		Scanner s4=new Scanner(System.in);
+    		    	System.out.println("Enter the File Name to be deleted:");
+    		    	String filename1=s4.next();
+    		    	FileOperations.delete(filename1);
+    		    	break;
+    		    	}
+                case "C" : 
+                    {
+            	    	Scanner s4= new Scanner (System.in);
+            	    	System.out.println("Enter the file name to be searched:");
+            	    	String filename2=s4.next();
+            	    	FileOperations.search(filename2);
+            	    	List l1 = FileOperations.readFileInList(filename2); 
+            		    Iterator<String> itr1 = l1.iterator(); 
+            		    while (itr1.hasNext()) 
+            		    	{
+            		    	System.out.println("The content of the file is:"+itr1.next()); 	
+            		    	}
+            	    	break;
+            	    	}
+                case "D" : {
+                    System.out.println("Showing main menu");
+                    showMainMenu();
+                    break;
+                }
+                default :
+                	System.out.println("Invalid Input. Please enter A, B, C or D");
+            }
+            showSubMenu();
+        }
+	public static void main(String args[])
+	{
 		System.out.println("*****************************");
 		System.out.println("Welcome to LockedMe.com!");
         System.out.println("*****************************");
-        //while loop is used to navigate back to the main content if user wants.
-        while(true)
-        {
-		System.out.println("--------Main Menu--------"); //showing the main menu
-		System.out.println("1. View the list of files inside current directory \n2. View UI options \n3. Exit from Application");
-		System.out.println("Enter your choice:"); //prompt the user to enter choice from the menu
-		Scanner sc=new Scanner(System.in);
-		String ch=sc.next();
-		if(ch.equals("1"))
-		{
-			//showFilesinascendingorder
-			String maindirpath = System.getProperty("user.dir");
-            
-	        // File object
-	        File maindir = new File(maindirpath);
-	           
-	        if(maindir.exists() && maindir.isDirectory())
-	        {
-	            // array for files and sub-directories 
-	            // of directory pointed by main_directory
-	            File arr[] = maindir.listFiles();
-	              
-	            System.out.println("**********************************************");
-	            System.out.println("All Files from current working directory (in ascending order) : " + maindir);
-	            System.out.println("**********************************************");
-	              
-	            // Calling recursive method
-	            FileOperations.RecursivePrint(arr,0,0); 
-	       }	
-	        System.out.println("Press 1 to return to the main menu 0 to exit");
-	        Scanner d=new Scanner(System.in);
-	        String re=d.next();
-	        if(re.equals("0"))
-	        {
-	        	System.exit(0);
-	        }
-	        else if (re.equals("1"))
-	        {
-	        	System.out.println();
-	        }
-	        else {
-	        	System.out.println("Invalid Input. Returning to Main menu..");
-	        }
-		}
-		else if (ch.equals("2"))
-				{
-	    	//UI options
-	    	System.out.println("A. Add a file \nB. Delete a specific file \nC. Search for a specific file ");
-	    	System.out.println("Enter your choice:");
-	    	Scanner s2=new Scanner(System.in);
-	    	String c= s2.next();
-	    	switch(c)
-	    	{
-	    	case "A":
-	    	{
-	    		//addfiles
-	    	Scanner sc1=new Scanner(System.in);
-			System.out.println("Enter File Name:");
-	        String filename=sc1.next();
-	        try {
-				FileOperations.createFileUsingFileClass(filename);
-			} catch (IOException e) {
-				System.err.println(e.getMessage());
-			}
-	        List l = FileOperations.readFileInList(filename); 
-		    Iterator<String> itr = l.iterator(); 
-		    while (itr.hasNext()) 
-		    	{
-		    	System.out.println("The content of the file is:"+itr.next()); 	
-		    	}
-	    	break;
-	    	}
-	    case "B":
-	    	//deleteFiles
-	    	{
-	    		Scanner sc3=new Scanner(System.in);
-	    	System.out.println("Enter the File Name to be deleted:");
-	    	String filename1=sc3.next();
-	    	FileOperations.delete(filename1);
-	    	break;
-	    	}
-	    case "C":
-	    	//searchFiles
-	    	{
-	    	Scanner sc4= new Scanner (System.in);
-	    	System.out.println("Enter the file name to be searched:");
-	    	String filename2=sc4.next();
-	    	FileOperations.search(filename2);
-	    	List l1 = FileOperations.readFileInList(filename2); 
-		    Iterator<String> itr1 = l1.iterator(); 
-		    while (itr1.hasNext()) 
-		    	{
-		    	System.out.println("The content of the file is:"+itr1.next()); 	
-		    	}
-	    	break;
-	    	}
-	    default:
-	    {
-	        System.out.println("Invalid Input");
-	        break;
-	    	}
-	    	}System.out.println("Press 1 to return to the main menu 0 to exit");
-	        Scanner d=new Scanner(System.in);
-	        String re=d.next();
-	        if(re.equals("0"))
-	        {
-	        	System.exit(0);
-	        }
-	        else if (re.equals("1"))
-	        {
-	        	System.out.println();
-	        }
-	        else {
-	        	System.out.println("Invalid Input.Showing Main Menu...");
-	        }
-				}
-		//close the application
-	    else if(ch.equals("3")) { 
-	    	System.exit(0);
-	    }else
-	    	{
-	        System.out.println("Invalid Input. Showing Main menu...");
-	    }
-        }
+        System.out.println("Developed by: Atreyee Mondal");
+        Test obj= new Test();
+        obj.showMainMenu();
 	}
 }
